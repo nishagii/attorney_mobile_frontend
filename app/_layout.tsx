7c96fc4ce6a9a3ec8900d946127fabb9e3803f87
Nishagi <jeewanthadeherath04@gmail.com>
2025-06-29 09:48:07 +0530

    ATTO-20 Add authentication layout and update navigation structure; include mini black logo and remove ranking gradient image

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,37 @@
 import { Stack } from "expo-router";
-import './globals.css';
+import "./globals.css";
 
 // this page is for groups
 
 export default function RootLayout() {
   return (
     <Stack>
+      {/* Auth screens */}
+      <Stack.Screen
+        name="auth"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      {/* Main app tabs */}
       <Stack.Screen
         name="(tabs)"
         options={{
           headerShown: false,
         }}
       />
+
+      {/* Other screens */}
       <Stack.Screen
         name="movie/[id]"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="case/[id]"
         options={{
           headerShown: false,
         }}
