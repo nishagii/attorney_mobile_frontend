dcd50719ef8e13ac524b736567cb75413a54f4f3
Nishagi <jeewanthadeherath04@gmail.com>
2025-07-04 18:48:27 +0530

    Refactor RootLayout component: add font loading logic and prevent splash screen from auto-hiding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,47 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { useFonts } from "expo-font";
+import {
+  Montserrat_100Thin,
+  Montserrat_200ExtraLight,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+} from "@expo-google-fonts/montserrat";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 // this page is for groups
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_100Thin,
+    Montserrat_200ExtraLight,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+    Montserrat_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <Stack
       screenOptions={{
