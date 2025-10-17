957b6ea39994142437768247c8c7c5a60ab83f64
RangikaaHerath001 <hmrsherath001@gmail.com>
2025-10-17 15:26:43 +0530

    Refactor import statements in AddClient screen for improved organization and readability

--- app/lawyer/(screens)/addclient.tsx
+++ app/lawyer/(screens)/addclient.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  ScrollView,
-  TouchableOpacity,
-  TextInput,
-  Alert,
-  StyleSheet,
-} from 'react-native';
+import { fonts } from '@/constants/fonts';
 import { Ionicons } from '@expo/vector-icons';
 import { router } from 'expo-router';
-import { fonts } from '@/constants/fonts';
+import React, { useState } from 'react';
+import {
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from 'react-native';
 
 const AddClient = () => {
   const user = {
